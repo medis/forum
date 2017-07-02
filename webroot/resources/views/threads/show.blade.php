00a4347c9dde45cc66bbefdb2808e148e2048d89
medis <aubuka@gmail.com>
2017-07-02 21:01:20 +0100

    create new threads

--- webroot/resources/views/threads/show.blade.php
+++ webroot/resources/views/threads/show.blade.php
@@ -23,5 +23,24 @@
             @endforeach
         </div>
     </div>
+
+    @if (auth()->check())
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-default">Post</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    @else
+        <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion</p>
+    @endif
 </div>
 @endsection
