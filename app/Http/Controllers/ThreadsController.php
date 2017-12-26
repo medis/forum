9657c19fe424cf1a257990f4c2cce325e1c18986
medis <aubuka@gmail.com>
2017-12-26 17:02:03 +0200

    global eager loading

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -116,7 +116,7 @@ class ThreadsController extends Controller
 
     public function getThreads($channel, $filters)
     {
-        $threads = Thread::with('channel')->latest()->filter($filters);
+        $threads = Thread::latest()->filter($filters);
 
         if ($channel->exists) {
             $threads->where('channel_id', $channel->id);
