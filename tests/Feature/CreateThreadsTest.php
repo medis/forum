c59553c2736a4368ab3323a385d0a85ceff4d3b1
medis <aubuka@gmail.com>
2017-12-27 00:14:15 +0200

    add ThreadPolicy to authorise actions, add global admin-like user

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -62,19 +62,21 @@ class CreateThreadsTest extends TestCase
     }
 
     /** @test */
-    public function guest_cannot_delete_threads()
+    public function unauthorised_users_may_not_delete_threads()
     {
         $this->withExceptionHandling();
         $thread = create('App\Thread');
-        $response = $this->delete($thread->path());
-        $response->assertRedirect('/login');
+        $this->delete($thread->path())->assertRedirect('/login');
+
+        $this->signIn();
+        $this->delete($thread->path())->assertStatus(403);
     }
 
     /** @test */
-    public function a_thread_can_be_deleted()
+    public function authorised_users_can_delete_threads()
     {
         $this->signIn();
-        $thread = create('App\Thread');
+        $thread = create('App\Thread', ['user_id' => auth()->id()]);
         $reply = create('App\Reply', ['thread_id' => $thread->id]);
 
         $response = $this->json('DELETE', $thread->path());
@@ -82,12 +84,6 @@ class CreateThreadsTest extends TestCase
 
         $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
         $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
-    }
-
-    /** @test */
-    public function threads_may_only_deleted_by_those_that_have_permission()
-    {
-        // TODO;
     }
 
     public function publishThread($overrides = [])
