976382f3eb504533a9936fbcfb7d43c27e7b78df
medis <aubuka@gmail.com>
2018-01-04 00:11:38 +0200

    initial user mention notification

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -6,6 +6,8 @@ use App\Reply;
 use App\Thread;
 use Illuminate\Http\Request;
 use App\Http\Requests\CreatePostRequest;
+use App\Notifications\YouWereMentioned;
+use App\User;
 
 class RepliesController extends Controller
 {
@@ -43,10 +45,22 @@ class RepliesController extends Controller
      */
     public function store($channelId, Thread $thread, CreatePostRequest $form)
     {
-        return $thread->addReply([
+        $reply = $thread->addReply([
             'body' => request('body'),
             'user_id' => auth()->id()
         ])->load('owner');
+
+        preg_match_all('/\@([^\s\.]+)/', $reply->body, $matches);
+
+        foreach ($matches[1] as $name) {
+            $user = User::whereName($name)->first();
+
+            if ($user) {
+                $user->notify(new YouWereMentioned($reply));
+            }
+        }
+
+        return $reply;
     }
 
     /**
