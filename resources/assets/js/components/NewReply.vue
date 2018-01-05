2306a68d7ca0f44c920e4e24c18819e14ef3864a
medis <aubuka@gmail.com>
2018-01-05 23:47:40 +0000

    instant username autocompletion

--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -25,6 +25,9 @@
 </template>
 
 <script>
+    import 'jquery.caret'
+    import 'at.js';
+
     export default {
 
         data() {
@@ -37,6 +40,20 @@
             signedIn() {
                 return window.App.signedIn;
             }
+        },
+
+        mounted() {
+            $('#body').atwho({
+                at: '@',
+                delay: 750,
+                callbacks: {
+                    remoteFilter: function(query, callback) {
+                        $.getJSON("/api/users", {name: query}, function(usernames) {
+                            callback(usernames)
+                        });
+                    }
+                }
+            });
         },
 
         methods: {
