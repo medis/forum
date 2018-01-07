d8c94d43c322b987184912a8336faa4e1e4f62ae
medis <aubuka@gmail.com>
2018-01-07 13:42:07 +0000

    tests for uploading avatar

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,4 @@ Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotification
 Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index');
 
 Route::get('api/users', 'Api\UsersController@index');
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarController@store')->middleware('auth');
