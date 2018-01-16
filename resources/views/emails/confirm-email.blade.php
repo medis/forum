feca1c97da04cfd20639b927d574855a342ba15d
medis <aubuka@gmail.com>
2018-01-16 23:28:41 +0000

    user email confirmation functionality

---
+++ resources/views/emails/confirm-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Introduction
+
+We just need you to confirm.
+
+@component('mail::button', ['url' => url('/register/confirm?token=' . $user->confirmation_token)])
+Confirm Email
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
