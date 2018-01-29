04b3328a1f68120ed5e478004d3b6f7e457d07ce
medis <aubuka@gmail.com>
2018-01-29 22:58:54 +0000

    update laravel to 5.5

--- app/Filters/Filters.php
+++ app/Filters/Filters.php
@@ -6,26 +6,30 @@ use Illuminate\Http\Request;
 
 abstract class Filters
 {
-  protected $request, $builder;
-  protected $filters = [];
+    protected $request;
+    protected $builder;
+    protected $filters = [];
 
-  public function __construct(Request $request) {
-    $this->request = $request;
-  }
-
-  public function apply($builder) {
-    $this->builder = $builder;
-
-    foreach ($this->getFilters() as $filter => $value) {
-      if (method_exists($this, $filter)) {
-        $this->$filter($value);
-      }
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
     }
 
-    return $this->builder;
-  }
+    public function apply($builder)
+    {
+        $this->builder = $builder;
 
-  public function getFilters() {
-    return $this->request->intersect($this->filters);
-  }
+        foreach ($this->getFilters() as $filter => $value) {
+            if (method_exists($this, $filter)) {
+                $this->$filter($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    public function getFilters()
+    {
+        return array_filter($this->request->only($this->filters));
+    }
 }
